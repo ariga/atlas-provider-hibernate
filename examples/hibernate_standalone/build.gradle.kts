ebdd451a702a0cd1dd0e241bef4b357814fe518f
dorav <dor.a2003@gmail.com>
2023-11-13 20:26:04 +0200

    added an example for how a different config would look like

--- examples/hibernate_standalone/build.gradle.kts
+++ examples/hibernate_standalone/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.ariga.SchemaTask
+
 plugins {
     kotlin("jvm") version "1.9.20"
     application
@@ -15,6 +17,9 @@ buildscript {
     repositories {
         mavenCentral()
     }
+    dependencies {
+        classpath("org.ariga:atlashibernate")
+    }
 }
 
 repositories {
@@ -23,9 +28,16 @@ repositories {
 
 dependencies {
     implementation("org.hibernate.orm:hibernate-core:6.3.1.Final")
+    implementation("org.ariga:atlashibernate")
     runtimeOnly("mysql:mysql-connector-java:8.0.33")
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+tasks.create("other-schema", SchemaTask::class.java) {
+    dialect = "MySQL"
+    packageFilter = listOf("org.example.model")
+    metadataBuilderClass = "org.example.MetadataBuilderImpl"
 }
