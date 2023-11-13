5bd564f294e3eb30786439f709520d0ba74f8fc2
dorav <dor.a2003@gmail.com>
2023-11-13 19:11:04 +0200

    sample_project: added simple Hibernate usage

---
+++ examples/hibernate_standalone/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    application
+    id("io.atlasgo.hibernate") version "0.1" apply true
+}
+
+group = "org.example"
+version = "1.0"
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.hibernate.orm:hibernate-core:6.3.1.Final")
+    runtimeOnly("mysql:mysql-connector-java:8.0.33")
+}
---
+++ examples/hibernate_standalone/src/main/java/org/example/minimodel/Event.java
@@ -0,0 +1,33 @@
+package org.example.minimodel;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity
+public class Event {
+    private String title;
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
+
