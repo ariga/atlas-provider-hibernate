78dac50a1919c346d779ee88b12c96018d9b2602
dorav <dor.a2003@gmail.com>
2023-11-19 11:37:13 +0200

    removed kotlin dependency from sample project

--- examples/hibernate_standalone/src/main/java/org/example/MetadataBuilderImpl.java
+++ examples/hibernate_standalone/src/main/java/org/example/MetadataBuilderImpl.java
@@ -4,14 +4,12 @@ import org.example.minimodel.Event;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.service.ServiceRegistry;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
 
 public class MetadataBuilderImpl implements Function<ServiceRegistry, Metadata> {
     @Override
-    @NotNull
-    public Metadata apply(@NotNull ServiceRegistry registry) {
+    public Metadata apply(ServiceRegistry registry) {
         return new MetadataSources(registry)
                 .addAnnotatedClasses(Event.class)
                 .buildMetadata();
