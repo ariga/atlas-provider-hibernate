abfd5eb61267eca0806a4fd05ceeee77014440b4
dorav <dor.a2003@gmail.com>
2023-11-28 14:27:27 +0200

    Revert "break test"
    This reverts commit 536407daf281ea15a454bd6e31824c3ca2f05284.

--- examples/with_spring_gradle/src/main/java/org/example/Location.java
+++ examples/with_spring_gradle/src/main/java/org/example/Location.java
@@ -12,13 +12,13 @@ public class Location {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    private String title2;
+    private String title;
 
-    public String getTitle2() {
-        return title2;
+    public String getTitle() {
+        return title;
     }
 
-    public void setTitle2(String title2) {
-        this.title2 = title2;
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
