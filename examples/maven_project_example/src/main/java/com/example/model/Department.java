b3d722786a7cb25cf9e4c758f6fa1b2278964656
dorav <dor.a2003@gmail.com>
2023-11-29 09:31:25 +0200

    examples: added a simple maven + hibernate project example

---
+++ examples/maven_project_example/src/main/java/com/example/model/Department.java
@@ -0,0 +1,52 @@
+package com.example.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+    @OneToMany(mappedBy="department",cascade= CascadeType.PERSIST)
+    private List<Employee> employees = new ArrayList<Employee>();
+
+    public Department() {
+        super();
+    }
+    public Department(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
