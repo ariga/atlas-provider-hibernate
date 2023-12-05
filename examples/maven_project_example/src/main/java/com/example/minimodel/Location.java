c66caa1e66900b3d6f218cdb1ddf237ce323c1e3
dorav <dor.a2003@gmail.com>
2023-12-05 16:39:27 +0200

    fixed test

--- examples/maven_project_example/src/main/java/com/example/minimodel/Location.java
+++ examples/maven_project_example/src/main/java/com/example/minimodel/Location.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Location {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public String title = "hello";
